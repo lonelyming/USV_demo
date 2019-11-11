36301cae78c739b66cb170aa2d2a74529aabf975
MoMing <122249853@qq.com>
2019-11-11 15:50:18 +0800

    fitst commit before write mag

---
+++ Inc/main.h
@@ -0,0 +1,91 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define GYRO_CS_Pin GPIO_PIN_13
+#define GYRO_CS_GPIO_Port GPIOC
+#define ACCEL_MAG_CS_Pin GPIO_PIN_15
+#define ACCEL_MAG_CS_GPIO_Port GPIOC
+#define MPU_CS_Pin GPIO_PIN_2
+#define MPU_CS_GPIO_Port GPIOC
+#define SPI_INT_SCK_Pin GPIO_PIN_5
+#define SPI_INT_SCK_GPIO_Port GPIOA
+#define SPI_INT_MISO_Pin GPIO_PIN_6
+#define SPI_INT_MISO_GPIO_Port GPIOA
+#define SPI_INT_MOSI_Pin GPIO_PIN_7
+#define SPI_INT_MOSI_GPIO_Port GPIOA
+#define FMU_UART7_RX_Pin GPIO_PIN_7
+#define FMU_UART7_RX_GPIO_Port GPIOE
+#define FMU_UART7_TX_Pin GPIO_PIN_8
+#define FMU_UART7_TX_GPIO_Port GPIOE
+#define FMC_LED_AMBER_Pin GPIO_PIN_12
+#define FMC_LED_AMBER_GPIO_Port GPIOE
+#define BARO_CS_Pin GPIO_PIN_7
+#define BARO_CS_GPIO_Port GPIOD
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ Src/usart.c
@@ -0,0 +1,135 @@
+/**
+  ******************************************************************************
+  * File Name          : USART.c
+  * Description        : This file provides code for the configuration
+  *                      of the USART instances.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "usart.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+UART_HandleTypeDef huart7;
+
+/* UART7 init function */
+void MX_UART7_Init(void)
+{
+
+  huart7.Instance = UART7;
+  huart7.Init.BaudRate = 115200;
+  huart7.Init.WordLength = UART_WORDLENGTH_8B;
+  huart7.Init.StopBits = UART_STOPBITS_1;
+  huart7.Init.Parity = UART_PARITY_NONE;
+  huart7.Init.Mode = UART_MODE_TX_RX;
+  huart7.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart7.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart7) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+
+void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(uartHandle->Instance==UART7)
+  {
+  /* USER CODE BEGIN UART7_MspInit 0 */
+
+  /* USER CODE END UART7_MspInit 0 */
+    /* UART7 clock enable */
+    __HAL_RCC_UART7_CLK_ENABLE();
+  
+    __HAL_RCC_GPIOE_CLK_ENABLE();
+    /**UART7 GPIO Configuration    
+    PE7     ------> UART7_RX
+    PE8     ------> UART7_TX 
+    */
+    GPIO_InitStruct.Pin = FMU_UART7_RX_Pin|FMU_UART7_TX_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF8_UART7;
+    HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN UART7_MspInit 1 */
+
+  /* USER CODE END UART7_MspInit 1 */
+  }
+}
+
+void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
+{
+
+  if(uartHandle->Instance==UART7)
+  {
+  /* USER CODE BEGIN UART7_MspDeInit 0 */
+
+  /* USER CODE END UART7_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_UART7_CLK_DISABLE();
+  
+    /**UART7 GPIO Configuration    
+    PE7     ------> UART7_RX
+    PE8     ------> UART7_TX 
+    */
+    HAL_GPIO_DeInit(GPIOE, FMU_UART7_RX_Pin|FMU_UART7_TX_Pin);
+
+  /* USER CODE BEGIN UART7_MspDeInit 1 */
+
+  /* USER CODE END UART7_MspDeInit 1 */
+  }
+} 
+
+/* USER CODE BEGIN 1 */
+/**************************函数声明***********************
+*函数名称：void Uart_Debug_Test (void)
+*输入参数：void		
+*返回参数：void
+*功    能：串口测试的测试函数
+*作    者：Ming
+*日    期：2019/11/8
+*************************************************************/
+void Uart_Polling_Test(void)
+{	
+	uint8_t test[] = "hello world from Polling \r\n";
+  if(HAL_UART_Transmit(&huart7, (uint8_t*)test, sizeof(test), 5000)!= HAL_OK)
+	{
+		Error_Handler();   
+	}
+}
+//printf 函数移植第一步：use microlib
+//printf 函数移植第二步；#include stdio.h
+//printf 函数移植第三部：重定向fputs函数
+/*******************************函数声明****************************************
+* 函数名称: void Uart_Printf_Test(void)
+* 输入参数: void
+* 返回参数: void
+* 功    能: 测试printf函数是否有效输出
+* 作    者: Ming
+* 日    期: 2019/11/8
+*******************************************************************************/ 
+void Uart_Printf_Test(void)
+{
+	printf("hello world from printf \r\n");
+}
+/* USER CODE END 1 */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
